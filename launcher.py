cbb938748eb91f87b39a1cc09927e88ae9d79ea9
NrgCristi <97525096+NrgCristi@users.noreply.github.com>
2022-09-20 09:47:48 +0300

    Add files via upload

---
+++ launcher.py
@@ -0,0 +1,8 @@
+import os
+import time
+
+path = ("please put your fortnite path")
+arguments = ("-NOSSLPINNING, -skippathcheck, -epicportal, -HTTP=WinNet")
+os.startFile("path, arguments, FortniteLauncher.exe")
+print("Closing in 1 minute")
+time.sleep(60)
